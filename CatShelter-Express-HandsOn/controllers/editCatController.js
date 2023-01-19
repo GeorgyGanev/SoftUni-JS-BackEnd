2acfcd4af92721e0ef285bcd1eee87f56880a3f7
Georgy Ganev <106162345+GeorgyGanev@users.noreply.github.com>
2023-01-19 11:31:39 +0200

    added breeds list to edit cat view

--- CatShelter-Express-HandsOn/controllers/editCatController.js
+++ CatShelter-Express-HandsOn/controllers/editCatController.js
@@ -1,18 +1,18 @@
 const router = require('express').Router();
-const {getList} = require('../services/dataService');
+const {getList, getBreeds} = require('../services/dataService');
  
 
 const cats = getList();
+const breeds = getBreeds();
 
 router.get('/:catId', (req, res) => {
     const catId = req.params.catId;
     const cat = cats.find(cat => cat.id == catId);
 
     console.log(cat);
-    
-    res.render('editCat', {cat});
+
+    res.render('editCat', {cat, breeds});
 })
 
 module.exports = router;
-
 
